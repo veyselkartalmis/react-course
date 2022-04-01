e6ec45cd48b1c95586f12d19ffb8fb4db3044488
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-04-01 16:48:36 +0300

    lifecycle

--- 04-state-setstate-and-lifecycle/src/components/Collapse.js
+++ 04-state-setstate-and-lifecycle/src/components/Collapse.js
@@ -15,10 +15,26 @@ class Collapse extends React.Component {
         /* Props ve state kullanımları benzerdir fakat onları ayıran en temel fark budur. */
     }
 
+    /*
+    state = {showContent: false}}  --> State bu şekilde daha kısa bir yazımla da tanımlanabilir 
+     */
+
     showMore = () => {
         /* Statei bulunduğu durumun tersine çevirir */
         /* setState metodu render() metodunun tekrar çalışması için tetikleyicidir */
         this.setState({showContent: !this.state.showContent});
+    }
+
+    /* Lifecyle Metotlar */
+
+    /* componentDidMount() metodu component'in render metodundan sonra çalışır */
+    componentDidMount(){
+        console.log("Component oluşturuldu");
+    }
+
+    /* componentDidUpdate() metodu component güncellendikçe çalışan metottur */
+    componentDidUpdate(){
+        console.log("Component güncellendi");
     }
 
     render() {
@@ -26,15 +42,18 @@ class Collapse extends React.Component {
             <div>
                 <p>
                     <button className="btn btn-primary w-100" onClick={this.showMore}>
-                        Link with href
+                        {/* Chilren propslara bu şekilde ulaşabilirsin */}
+                        {React.Children.map(this.props.children, children => children.props.cardTitle)}
                     </button>
                 </p>
                 {
                     /* Eğer state true ise cardı göster değilse kapat */
                     this.state.showContent ? (
                         <div className="collapse show">
-                            {this.props.children} {/* Parent element Collapse olduğu için içerisine eklenen Card verisi children olarak bu şekilde yazılır */}
-                            {this.state.showContent}
+                            {/* {this.props.children} Parent element Collapse olduğu için içerisine eklenen Card verisi children olarak bu şekilde yazılır */}
+
+                            {/* İki şekilde de propslar çekilebilir üstte ve altta olduğu gibi fakat aşağıda bulunan yazım daha makul. */}
+                            {React.Children.map(this.props.children, children => children)}
                         </div>
                     ) : null
                 }
